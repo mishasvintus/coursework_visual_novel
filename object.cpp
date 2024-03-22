ba79ab703d824b8e830918b200c3168fbda392f9
0ficus <127107424+0ficus@users.noreply.github.com>
2024-03-22 15:57:29 +0300

    Create object.cpp

---
+++ object.cpp
@@ -0,0 +1,33 @@
+#include "object.h"
+
+DialogueBox::DialogueBox() : replica_(DEFAULT_REPLICA), speaker_(DEFAULT_SPEAKER) {
+}
+
+DialogueBox::DialogueBox(const std::string_view &replica, const std::string_view &speaker) : replica_(replica),
+                                                                                             speaker_(speaker) {
+}
+
+DialogueBox::DialogueBox(const DialogueBox &other) : replica_(other.replica_), speaker_(other.speaker_) {
+}
+
+DialogueBox::DialogueBox(const DialogueBox&& other) : replica_(other.replica_), speaker_(other.speaker_) {
+}
+
+DialogueBox::DialogueBox(DialogueBox&& other) : replica_(other.replica_), speaker_(other.speaker_) {
+}
+
+std::string_view DialogueBox::GetReplica() {
+    return replica_;
+}
+
+std::string_view DialogueBox::GetSpeaker() {
+    return speaker_;
+}
+
+void DialogueBox::SetReplica(const std::string_view& replica) {
+    replica_ = replica;
+}
+
+void DialogueBox::SetSpeaker(const std::string_view& speaker) {
+    speaker_ = speaker;
+}
