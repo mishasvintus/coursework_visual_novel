35d59eaacb3ae14a7bacad13ebfdac3ff0e3f4f6
0ficus <127107424+0ficus@users.noreply.github.com>
2024-03-22 15:56:23 +0300

    Update game_engine.h
    add method that will be create dialogue box in latter

--- game_engine.h
+++ game_engine.h
@@ -1,22 +1,33 @@
-#include <SFML/Graphics.hpp>
+#include "object.h"
+
+#include <vector>
 
 namespace ge {
-
-    const std::string DEFAULT_PROJECT_NAME = "Project";
 
     class Application {
     public:
         explicit Application();
+
         explicit Application(const std::string &project_name);
+
         explicit Application(const Application &other) = delete;
+
         explicit Application(const Application &&other) = delete;
+
         ~Application() = default;
 
         void Finish();
+        
+        void CreateDialogueBox(const std::string_view& replica, const std::string_view& speaker) {
+            //// will be latter, we must realise all methods in dialogue box for using it like abstract class
+        }
 
     private:
         static void ApplyRendering(const std::string &project_name);
 
+        const std::string DEFAULT_PROJECT_NAME = "Project";
+
         sf::Thread rendering_thread_;
+        std::vector<Object> objects_;
     };
 }
