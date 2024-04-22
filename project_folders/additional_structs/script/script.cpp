ee934cda2991d5118d002c5dccc01017536651ac
0ficus <127107424+0ficus@users.noreply.github.com>
2024-04-23 02:02:06 +0300

    init

--- project_folders/additional_structs/script/script.cpp
+++ project_folders/additional_structs/script/script.cpp
@@ -1,1 +1,14 @@
 #include "script.h"
+
+ge::Script::Script(const std::unordered_map<std::string, Chapter> &chapters)
+    : chapters_(chapters) {
+}
+
+ge::Script::Script(Script &&script) noexcept
+    : chapters_(std::move(script.chapters_)) {
+}
+
+ge::Script &ge::Script::operator=(Script &&script) noexcept {
+    chapters_ = std::move(script.chapters_);
+    return *this;
+}
