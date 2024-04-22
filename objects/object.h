73cc0b84bf3790200d5631e4d8765ad50b565d90
mishasvintus <pokemongonchar@gmail.com>
2024-04-22 19:42:26 +0300

    getSpriteVector must give sprites relative to windows size

--- objects/object.h
+++ objects/object.h
@@ -1,6 +1,12 @@
 #pragma once
 
+
+#include <SFML/System/Vector2.hpp>
+#include <SFML/Graphics.hpp>
+
 class Object {
+public:
+    virtual ~Object() = default;
 private:
-    virtual void get_sprite_vector();
+    virtual std::vector<sf::Sprite> getSpriteVector(sf::Vector2i window_size) = 0;
 };
