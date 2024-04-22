bd16a843dbc113c2ffa6cad9a836b1a0287e2587
mishasvintus <pokemongonchar@gmail.com>
2024-04-22 18:01:34 +0300

    Добавил файлы для игнора

---
+++ objects/user_objects/visual_novel/visual_novel.cpp
@@ -0,0 +1,1 @@
+#include "visual_novel.h"
---
+++ objects/user_objects/visual_novel/visual_novel.h
@@ -0,0 +1,12 @@
+#pragma once
+
+class VisualNovel {
+public:
+    bool set_about_authors(std::string);
+    bool set_script()
+    bool run();
+    std::string about_authors;
+private:
+    _about_authors;
+};
+
