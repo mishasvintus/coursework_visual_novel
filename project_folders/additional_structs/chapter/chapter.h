c051525b909c752ab8b3303c8b92832da83a2875
0ficus <127107424+0ficus@users.noreply.github.com>
2024-04-23 02:01:36 +0300

    make main methods by rule of 5

--- project_folders/additional_structs/chapter/chapter.h
+++ project_folders/additional_structs/chapter/chapter.h
@@ -4,7 +4,21 @@
 
 namespace ge {
     struct Chapter {
-        std::string chapter_name;
-        std::vector<Scene> scenes;
+        Chapter() = default;
+
+        Chapter(const Chapter &chapter) = default;
+
+        Chapter(std::string chapter_name, const std::vector<Scene> &scenes);
+
+        Chapter(Chapter &&chapter) noexcept;
+
+        ~Chapter() = default;
+
+        Chapter &operator=(const Chapter &chapter) = default;
+
+        Chapter &operator=(Chapter &&chapter) noexcept;
+
+        std::string chapter_name_;
+        std::vector<Scene> scenes_;
     };
 }
