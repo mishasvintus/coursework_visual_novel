97dfdecb40453252c28cc86c52b88aaa7aa73808
0ficus <127107424+0ficus@users.noreply.github.com>
2024-04-23 02:03:46 +0300

    made main methods by rule of 5

--- project_folders/objects/user_objects/actions/actions.h
+++ project_folders/objects/user_objects/actions/actions.h
@@ -1,29 +1,40 @@
 #pragma once
 
-#include <SFML/System/Vector2.hpp>
 #include "object.h"
 
-namespace ge {
-    class Chapter;
+#include <SFML/System/Vector2.hpp>
 
+namespace ge {
     class Action : Object {
     public:
         Action();
+
+        Action(const sf::Vector2i &coors, std::string text, std::string chapter_name_to_go);
 
         Action(const Action &action);
 
         Action(Action &action);
 
-        Action(Action &&action);
+        Action(Action &&action) noexcept;
 
-        ~Action() = default;
+        ~Action() override = default;
+
+        Action &operator=(const Action &action);
+
+        Action &operator=(Action &&action) noexcept;
 
     private:
-        sf::Vector2i coords_;
-        std::string text_;
-        std::string chapter_name_to_go_;
+        std::vector<sf::Sprite> getSpriteVector(const sf::Vector2u &window_size) override {
+            //TODO: REALISE
+            return {};
+        }
 
-        std::vector<sf::Sprite> getSpriteVector(const sf::Vector2i& window_size) override;
+        const sf::Vector2u COORS_LOW_LIMIT = {0, 0};
+        const sf::Vector2u COORS_HIGH_LIMIT = {10000, 10000};
+                    //TODO: не константа, потому что sf::VideoMode нестатичен, переделать как-то
+
+        sf::Vector2u coors_;
+        std::string text_; //// возможно лучше string_view
+        std::string chapter_name_to_go_; //// возможно лучше string_view
     };
 }
-
