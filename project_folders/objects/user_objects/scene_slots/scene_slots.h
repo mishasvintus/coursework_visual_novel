996dd20fe4e082a04158a80bceb38728e624ef9e
0ficus <127107424+0ficus@users.noreply.github.com>
2024-04-23 02:06:55 +0300

    made main methods by rule of 5

--- project_folders/objects/user_objects/scene_slots/scene_slots.h
+++ project_folders/objects/user_objects/scene_slots/scene_slots.h
@@ -11,24 +11,32 @@ namespace ge {
 
         SceneSlots(SceneSlots &scene_slots);
 
-        SceneSlots(SceneSlots &&sceneSlots);
+        SceneSlots(SceneSlots &&scene_slots) noexcept ;
 
         SceneSlots(unsigned int quantity_of_slots, const std::vector<std::string> &pictures_in_slots);
 
-        ~SceneSlots() = default;
+        ~SceneSlots() override = default;
+
+        SceneSlots& operator=(const SceneSlots& scene_slots);
+
+        SceneSlots& operator=(SceneSlots&& scene_slots) noexcept;
 
         bool setQuantityOfSlots(unsigned int quantity_of_slots);
 
         bool setPicturesInSlots(const std::vector<std::string> &pictures_in_slots);
 
-        unsigned int getQuantityOfSlots();
+        [[nodiscard]] unsigned int getQuantityOfSlots() const;
 
         std::vector<std::string> getPicturesInSlots();
 
     private:
+        std::vector<sf::Sprite> getSpriteVector(const sf::Vector2u &window_size) override {
+            return {}; ///TODO: реализовать
+        }
+
+        static constexpr unsigned int DEFAULT_COUNT_SLOTS = 5;
+        static constexpr unsigned int UPPER_BOUND_COUNT_SLOTS = 10;
         unsigned int quantity_of_slots_;
         std::vector<std::string> pictures_in_slots_;
-
-        std::vector<sf::Sprite> getSpriteVector(const sf::Vector2i &window_size) override;
     };
 }
