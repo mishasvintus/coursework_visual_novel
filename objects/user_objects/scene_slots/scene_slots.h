99bd0ddd341e1f60c3c6f9f78fda1e52750d8f29
mishasvintus <pokemongonchar@gmail.com>
2024-04-22 21:44:15 +0300

    added interface for scene_slots

--- objects/user_objects/scene_slots/scene_slots.h
+++ objects/user_objects/scene_slots/scene_slots.h
@@ -5,8 +5,30 @@
 namespace ge {
     class SceneSlots : Object {
     public:
+        SceneSlots();
+
+        SceneSlots(const SceneSlots &scene_slots);
+
+        SceneSlots(SceneSlots &scene_slots);
+
+        SceneSlots(SceneSlots &&sceneSlots);
+
+        SceneSlots(unsigned int quantity_of_slots, const std::vector<std::string> &pictures_in_slots);
+
+        ~SceneSlots() = default;
+
+        bool setQuantityOfSlots(unsigned int quantity_of_slots);
+
+        bool setPicturesInSlots(const std::vector<std::string> &pictures_in_slots);
+
+        unsigned int getQuantityOfSlots();
+
+        std::vector<std::string> getPicturesInSlots();
 
     private:
-        std::vector<sf::Sprite> getSpriteVector(const sf::Vector2i& window_size) override;
+        unsigned int quantity_of_slots_;
+        std::vector<std::string> pictures_in_slots_;
+
+        std::vector<sf::Sprite> getSpriteVector(const sf::Vector2i &window_size) override;
     };
 }
