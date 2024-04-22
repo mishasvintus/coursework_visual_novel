785e0e6a60a9ccfc5e3f49f31e72c4a39cb9e9b4
0ficus <127107424+0ficus@users.noreply.github.com>
2024-04-23 02:01:05 +0300

    init

--- project_folders/additional_structs/chapter/chapter.cpp
+++ project_folders/additional_structs/chapter/chapter.cpp
@@ -1,1 +1,20 @@
 #include "chapter.h"
+
+#include <utility>
+
+ge::Chapter::Chapter(std::string chapter_name, const std::vector<Scene> &scenes)
+    : chapter_name_(std::move(chapter_name))
+    , scenes_(scenes) {
+}
+
+ge::Chapter::Chapter(Chapter &&chapter) noexcept
+    : chapter_name_(std::move(chapter.chapter_name_))
+    , scenes_(std::move(chapter.scenes_)){
+}
+
+ge::Chapter &ge::Chapter::operator=(Chapter &&chapter) noexcept {
+    chapter_name_= chapter.chapter_name_;
+    scenes_ = chapter.scenes_;
+    return *this;
+}
+
