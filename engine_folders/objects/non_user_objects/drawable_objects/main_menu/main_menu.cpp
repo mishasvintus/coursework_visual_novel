7378ceead8166aaab61400d335350a39fa63aa47
0ficus <sancho.white@bk.ru>
2024-04-23 21:00:30 +0300

    init methods main_menu "
    q

--- engine_folders/objects/non_user_objects/drawable_objects/main_menu/main_menu.cpp
+++ engine_folders/objects/non_user_objects/drawable_objects/main_menu/main_menu.cpp
@@ -1,1 +1,41 @@
 #include "main_menu.h"
+
+void checkingCorrectness(const unsigned int selected_button, const unsigned int upper_limit) {
+    if (selected_button > upper_limit - 1) {
+        throw std::invalid_argument("so big number in selected_button\n");
+    }
+}
+
+ge::MainMenu::MainMenu(const MainMenu &main_menu)
+    : selected_button_(main_menu.selected_button_) {
+}
+
+ge::MainMenu::MainMenu(MainMenu &main_menu)
+    : selected_button_(main_menu.selected_button_) {
+}
+
+ge::MainMenu::MainMenu(MainMenu &&main_menu) noexcept
+    : selected_button_(main_menu.selected_button_) {
+}
+
+ge::MainMenu &ge::MainMenu::operator=(const MainMenu &main_menu) {
+    selected_button_ = main_menu.selected_button_;
+    return *this;
+}
+
+ge::MainMenu &ge::MainMenu::operator=(MainMenu &&main_menu) {
+    selected_button_ = main_menu.selected_button_;
+    return *this;
+}
+
+void ge::MainMenu::MoveUp() {
+    if (selected_button_ == UP) {
+        return;
+    }
+    --selected_button_;
+}
+
+void ge::MainMenu::MoveDown() {
+    selected_button_ = std::min(selected_button_ + 1, QUANTITY_OF_BUTTONS_ - 1);
+}
+
--- engine_folders/objects/non_user_objects/drawable_objects/main_menu/main_menu.h
+++ engine_folders/objects/non_user_objects/drawable_objects/main_menu/main_menu.h
@@ -5,18 +5,33 @@
 
 namespace ge {
     class MainMenu : Drawable {
-        MainMenu();
+    public:
+        MainMenu() = default;
+
         MainMenu(const MainMenu &main_menu);
+
         MainMenu(MainMenu &main_menu);
-        MainMenu(MainMenu &&main_menu);
-        ~MainMenu() = default;
+
+        MainMenu(MainMenu &&main_menu) noexcept;
+
+        ~MainMenu() override = default;
+
+        MainMenu& operator=(const MainMenu& main_menu);
+
+        MainMenu& operator=(MainMenu&& main_menu) noexcept ;
 
         void MoveUp();
-        void MoveDown();
-    private:
-        unsigned int selected_button;
-        std::vector<sf::Sprite> getSpriteVector(const sf::Vector2u& window_size) override;
-        const unsigned int QUANTITY_OF_BUTTONS_ = 5;
 
+        void MoveDown();
+
+    private:
+        std::vector<sf::Sprite> getSpriteVector(const sf::Vector2u &window_size) override {
+            return {}; ///TODO: реализовать
+        }
+
+        static constexpr unsigned int UP = 0;
+        static constexpr unsigned int QUANTITY_OF_BUTTONS_ = 5;
+
+        unsigned int selected_button_ = 0;
     };
 }
