c430af5908c0a44795a8736280b8b1f69ac33cf9
0ficus <sancho.white@bk.ru>
2024-04-25 15:36:52 +0300

    touched scene_changes_sfml_basic

--- engine_folders/objects/non_user_objects/sfml_basis/scene_changes_sfml_basic/scene_changes_sfml_basic.h
+++ engine_folders/objects/non_user_objects/sfml_basis/scene_changes_sfml_basic/scene_changes_sfml_basic.h
@@ -4,7 +4,21 @@
 
 namespace ge {
     class SceneChangesSfmlBasic : public ge::SfmlBasis {
+    public:
+        SceneChangesSfmlBasic() = default;
+
         ~SceneChangesSfmlBasic() override = default;
-        void draw(sf::RenderWindow &window) override {} /// TODO: реализовать
+
+        void draw(sf::RenderWindow &window) override {
+        } /// TODO: реализовать
+
+        sf::Font replica_font_;
+        sf::Font speaker_font_;
+        sf::RectangleShape dialogue_box_shape;
+        sf::Sprite background_sprite_;
+        sf::Texture background_texture_;
+        std::vector<sf::Sprite> slots_spites_;
+        std::vector<sf::Texture> slots_textue_;
+        sf::Text button_;
     };
 }
