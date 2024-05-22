742106adaa34eeace0465d35a342893d820cf6d8
mishasvintus <pokemongonchar@gmail.com>
2024-05-22 04:02:53 +0300

    added more files to factory

--- game/chapter_factory.h
+++ game/chapter_factory.h
@@ -36,7 +36,10 @@ struct ChapterFactory {
     const std::string NATALYA_NIKOLAEVNA_ANGRY_MUTELY = "game/game_data/images/character_models/natalya_nikolaevna/angry_mutely.png";
     const std::string NATALYA_NIKOLAEVNA_ANGRY_TALKING = "game/game_data/images/character_models/natalya_nikolaevna/angry_talking.png";
 
-
+    const std::string OLEG_SAD_MUTELY = "game/game_data/images/character_models/oleg/sad_mutely.png";
+    const std::string OLEG_SAD_TALKING = "game/game_data/images/character_models/oleg/sad_talking.png";
+    const std::string OLEG_BASE_MUTELY = "game/game_data/images/character_models/oleg/base_mutely.png";
+    const std::string OLEG_BASE_TALKING = "game/game_data/images/character_models/oleg/base_talking.png";
     const std::string OLEG_GOP_MUTELY = "game/game_data/images/character_models/oleg/gop_mutely.png";
     const std::string OLEG_GOP_TALKING = "game/game_data/images/character_models/oleg/gop_talking.png";
 
@@ -63,16 +66,37 @@ struct ChapterFactory {
     const std::string SASHA_HAPPY_TALKING = "game/game_data/images/character_models/male_character/happy_talking.png";
     const std::string SASHA_SURPRISED_MUTELY = "game/game_data/images/character_models/male_character/surprised_mutely.png";
     const std::string SASHA_SURPRISED_TALKING = "game/game_data/images/character_models/male_character/surprised_talking.png";
+    const std::string SASHA_ANGRY_MUTELY = "game/game_data/images/character_models/male_character/angry_mutely.png";
+    const std::string SASHA_ANGRY_TALKING = "game/game_data/images/character_models/male_character/angry_talking.png";
+    const std::string SASHA_SAD_MUTELY = "game/game_data/images/character_models/male_character/sad_mutely.png";
+    const std::string SASHA_SAD_TALKING = "game/game_data/images/character_models/male_character/sad_talking.png";
+
 
     const std::string KATE_BASE_MUTELY = "game/game_data/images/character_models/katerina/base_mutely.png";
     const std::string KATE_BASE_TALKING = "game/game_data/images/character_models/katerina/base_talking.png";
     const std::string KATE_ANGRY_MUTELY = "game/game_data/images/character_models/katerina/angry_mutely.png";
     const std::string KATE_ANGRY_TALKING = "game/game_data/images/character_models/katerina/angry_talking.png";
-
-
     const std::string KATE_SURPRISED_MUTELY = "game/game_data/images/character_models/katerina/surprised_mutely.png";
     const std::string KATE_SURPRISED_TALKING = "game/game_data/images/character_models/katerina/surprised_talking.png";
+    const std::string KATE_HAPPY_MUTELY = "game/game_data/images/character_models/katerina/happy_mutely.png";
+    const std::string KATE_HAPPY_TALKING = "game/game_data/images/character_models/katerina/happy_talking.png";
 
+
+    const std::string RAVSHAN_BASE_MUTELY = "game/game_data/images/character_models/ravshan/base_mutely.png";
+    const std::string RAVSHAN_BASE_TALKING = "game/game_data/images/character_models/ravshan/base_talking.png";
+    const std::string RAVSHAN_SURPRISED_MUTELY = "game/game_data/images/character_models/ravshan/surprised_mutely.png";
+    const std::string RAVSHAN_SURPRISED_TALKING = "game/game_data/images/character_models/ravshan/surprised_talking.png";
+    const std::string RAVSHAN_HAPPY_MUTELY = "game/game_data/images/character_models/ravshan/happy_mutely.png";
+    const std::string RAVSHAN_HAPPY_TALKING = "game/game_data/images/character_models/ravshan/happy_talking.png";
+    const std::string RAVSHAN_ANGRY_MUTELY = "game/game_data/images/character_models/ravshan/angry_mutely.png";
+    const std::string RAVSHAN_ANGRY_TALKING = "game/game_data/images/character_models/ravshan/angry_talking.png";
+
+    const std::string DIMA_BASE_MUTELY = "game/game_data/images/character_models/dima/base_mutely.png";
+    const std::string DIMA_BASE_TALKING = "game/game_data/images/character_models/dima/base_talking.png";
+    const std::string DIMA_HAPPY_MUTELY = "game/game_data/images/character_models/dima/happy_mutely.png";
+    const std::string DIMA_HAPPY_TALKING = "game/game_data/images/character_models/dima/happy_talking.png";
+    const std::string DIMA_SURPRISED_MUTELY = "game/game_data/images/character_models/dima/surprised_mutely.png";
+    const std::string DIMA_SURPRISED_TALKING = "game/game_data/images/character_models/dima/surprised_talking.png";
 
     const std::string CAT = "game/game_data/images/mascot/cat.png";
 };
